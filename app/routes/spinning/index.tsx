115d9994a3439d16592b66ed5c4752d5caefb732
Marcio <marcioscar@gmail.com>
2023-03-20 16:32:00 -0300

    cores

--- app/routes/spinning/index.tsx
+++ app/routes/spinning/index.tsx
@@ -105,7 +105,7 @@ export default function Index() {
                   className={
                     "" + transition.state === "loading"
                       ? "rounded-xl mt-2  bg-blue-600 px-3 py-2 text-white font-semibold"
-                      : "rounded-xl mt-2  bg-orange-600 px-3 py-2 text-white font-semibold hover:bg-orange-400 hover:-translate-y-1"
+                      : "rounded-xl mt-2  bg-green-400 px-3 py-2  font-semibold hover:bg-orange-400 hover:-translate-y-1"
                   }>
                   {transition.state === "submitting"
                     ? "Localizando..."
